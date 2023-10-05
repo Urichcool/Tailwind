421cde5366d429850c6898bfa2986390ed4ce411
Yurii Novikov <104870932+Urichcool@users.noreply.github.com>
2023-10-05 23:14:00 +0200

    created-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import styles from "./App.module.scss";
 
 function App() {
-  return <div className={styles.parent}>Hello</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className={styles.parent}>
+      <h1 className={styles.heading}>Hello</h1>
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button className={styles.button}>Login</button>
+    </div>
+  );
 }
 
 export default App;
